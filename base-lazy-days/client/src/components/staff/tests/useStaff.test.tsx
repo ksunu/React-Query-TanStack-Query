8baf66bb2ca5a8b2ffe39167e9b1ba778cf8a03e
ksunu <soonk@outlook.com>
2024-05-04 14:51:37 +0900

    practicing testing react-query

--- base-lazy-days/client/src/components/staff/tests/useStaff.test.tsx
+++ base-lazy-days/client/src/components/staff/tests/useStaff.test.tsx
@@ -1,9 +1,20 @@
-// import { act, renderHook, waitFor } from "@testing-library/react";
+import { act, renderHook, waitFor } from "@testing-library/react";
 
 import { useStaff } from "../hooks/useStaff";
 
-// import { createQueryClientWrapper } from "@/test-utils";
+import { createQueryClientWrapper } from "@/test-utils";
 
 test("filter staff", async () => {
-  // the magic happens here
+  const { result } = renderHook(() => useStaff(), {
+    wrapper: createQueryClientWrapper()
+  })
+
+  // wait for staff to populate
+  await waitFor(() => expect(result.current.staff).toHaveLength(4))
+
+  // set to filter for only staff who gives massage
+  act(() => result.current.setFilter("facial"))
+
+  // wait for staff list to display only 3
+  await waitFor(() => expect(result.current.staff).toHaveLength(3))
 });
--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -1,4 +1,4 @@
-import { MutationCache, QueryCache, QueryClient } from "@tanstack/react-query"
+import { MutationCache, QueryCache, QueryClient, QueryClientConfig } from "@tanstack/react-query"
 
 import { toast } from "@/components/app/toast"
 
@@ -21,7 +21,7 @@ function errorHandler(title: string) {
   }
 }
 
-export const queryClient = new QueryClient({
+export const queryClientOptions: QueryClientConfig = {
   defaultOptions: {
     queries: {
       staleTime: 600000,
@@ -41,4 +41,6 @@ export const queryClient = new QueryClient({
       errorHandler(title)
     }
   })
-})
+}
+
+export const queryClient = new QueryClient(queryClientOptions)
